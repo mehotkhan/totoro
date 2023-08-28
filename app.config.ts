8604691dfd032ac269cfc8fc130b9834a4a759ad
Ali Zemani <mehot1@gmail.com>
2023-08-28 18:27:55 +0330

    fix gitscus

--- app.config.ts
+++ app.config.ts
@@ -11,7 +11,13 @@ export default defineAppConfig({
     twitter: "",
     giscus: true, // enable  giscus comment systems
     giscus_term: "به توتورو خوش آمدید :)",
+    giscus_category: "General",
     giscus_repo: "mehotkhan/totoro",
+    giscus_repo_id: "R_kgDOKK9Djw",
+    giscus_category_id: "DIC_kwDOKK9Dj84CY100",
+    giscus_mapping: "title",
+    giscus_theme: "light",
+    giscus_reaction: "1",
   },
   ui: {
     primary: "gray",
